7fc400db5817dc9c88ec188fc2914de63ed89e33
Parthendo <parthofficio28@gmail.com>
2017-06-26 17:57:43 +0530

    LBP 1

--- Cultural_Fest.m
+++
@@ -1,8 +0,0 @@
-IMDS = imageDatastore('D:\Cultural Fest','IncludeSubfolders',true,'LabelSource','foldernames');
-[trainingSet,testSet] = splitEachLabel(IMDS,70);
-trainingSetLength = length(trainingSet.Files);
-testSetLength = length(testSet.Files);
-training_features = [];
-test_features = [];
-
-
